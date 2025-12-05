0810674111ae1341516eb9ad754d81ebb4cc9e85
Xander <xandwrp@gmail.com>
2025-12-05 10:39:33 -0700

    Site setup with asciinema

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+	import 'asciinema-player/dist/bundle/asciinema-player.css';
+
+	let container;
+
+	onMount(async () => {
+		const AsciinemaPlayer = await import('asciinema-player');
+		AsciinemaPlayer.create('/demos/demo.cast', container);
+	});
+</script>
+
+<div bind:this={container}></div>
