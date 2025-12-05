f77f1389e7d4d50c1a964d61ca73994fd8f8cc5e
Xander <xandwrp@gmail.com>
2025-12-05 11:17:46 -0700

    Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,16 @@
 
 	onMount(async () => {
 		const AsciinemaPlayer = await import('asciinema-player');
-		AsciinemaPlayer.create('/demos/demo.cast', container);
+		AsciinemaPlayer.create('/demos/demo.cast', container, {
+			autoPlay: true,
+			controls: false,
+			loop: false,
+			fit: "height",
+			terminalFontFamily: 'monospace'
+		});
 	});
 </script>
 
 <div class="flex flex-1 items-center justify-center py-12 px-4">
-	<div class="max-w-2xl w-full" bind:this={container}></div>
+	<div class="max-w-2xl w-full h-[500px] overflow-y-auto" bind:this={container}></div>
 </div>
